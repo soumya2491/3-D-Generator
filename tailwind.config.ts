6af3e5d63a20504a23624060c46a139d08c18ab7
soumya2491 <soumyachopra901@gmail.com>
2025-08-29 20:52:58 +0530

    Added basic tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+export default {
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
+	],
+	prefix: "",
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		
+	},
+	plugins: [require("tailwindcss-animate")],
+} satisfies Config;

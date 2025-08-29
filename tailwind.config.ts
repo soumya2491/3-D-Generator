d0a3ba9e7363ffad4d903c1c79db1c82f8a1ea04
soumya2491 <soumyachopra901@gmail.com>
2025-08-29 21:06:15 +0530

    Done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,6 @@ export default {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out'
 			}
-				
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
